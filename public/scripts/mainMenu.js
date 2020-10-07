593a1358bc93baab9c94a0092655a3f7d9ba80b8
ttorino1999 <ttorino1999@yahoo.com>
2020-10-07 15:26:00 -0500

    cleaned ejs and made js file

---
+++ public/scripts/mainMenu.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+	getStartTransactionActionElement().addEventListener("click", () => displayError("Functionality has not yet been implemented."));
+
+	getViewProductsActionElement().addEventListener("click", () => window.location.assign("/productListing"));
+
+	getCreateEmployeeActionElement().addEventListener("click", () => window.location.assign("/employeeDetail"));
+
+	getProductSalesReportActionElement().addEventListener("click", () => displayError("Functionality has not yet been implemented."));
+
+	getCashierSalesReportActionElement().addEventListener("click", () => displayError("Functionality has not yet been implemented."));
+});
+
+function getStartTransactionActionElement() {
+	return document.getElementById("startTransactionButton");
+}
+
+function getViewProductsActionElement() {
+	return document.getElementById("viewProductsButton");
+}
+
+function getCreateEmployeeActionElement() {
+	return document.getElementById("createEmployeeButton");
+}
+
+function getProductSalesReportActionElement() {
+	return document.getElementById("productSalesReportButton");
+}
+
+function getCashierSalesReportActionElement() {
+	return document.getElementById("cashierSalesReportButton");
+}
