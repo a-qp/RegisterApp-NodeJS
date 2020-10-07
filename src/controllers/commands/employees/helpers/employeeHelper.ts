c7d1e31908312d4cbf033dd0944bfbf81481f8d8
ttorino1999 <ttorino1999@yahoo.com>
2020-10-07 11:39:46 -0500

    fixed the ugly code in employeehelper

--- src/controllers/commands/employees/helpers/employeeHelper.ts
+++ src/controllers/commands/employees/helpers/employeeHelper.ts
@@ -4,17 +4,17 @@ import { EmployeeModel } from "../../models/employeeModel";
 import { EmployeeClassification } from "../../models/constants/entityTypes";
 
 export const hashString = (toHash: string): string => {
-const hash = crypto.createHash("sha256");
-hash.update(toHash);
-return hash.digest("hex"); // TODO: Look at https://nodejs.org/docs/latest-v12.x/api/crypto.html#crypto_crypto_createhash_algorithm_options as one option
+    const hash = crypto.createHash("sha256");
+    hash.update(toHash);
+    return hash.digest("hex"); // TODO: Look at https://nodejs.org/docs/latest-v12.x/api/crypto.html#crypto_crypto_createhash_algorithm_options as one option
 };
 
 export const isElevatedUser = (employeeClassification: EmployeeClassification): boolean => {
-if (EmployeeClassification.NotDefined) {
-return false; // TODO: Determine if an employee is an elevated user by their classification
-}
-else
-return true;
+    if (EmployeeClassification.NotDefined) {
+        return false; // TODO: Determine if an employee is an elevated user by their classification
+    }
+    else
+        return true;
 };
 
 export const makeEmployeeId = (id: number): string => {
